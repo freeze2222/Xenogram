4d608a280246e5889a2510d4bb0925b718c124f4
Freeze2222 <opop1094@gmail.com>
2022-04-25 00:06:47 +0300

    Experiment 1

---
+++ app/src/main/java/com/FinalP/finalchat/fragments/ProfileFragment.java
@@ -0,0 +1,28 @@
+package com.FinalP.finalchat.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.FinalP.finalchat.R;
+
+public class ProfileFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
