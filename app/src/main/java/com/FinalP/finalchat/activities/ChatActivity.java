736f00a93284f95c3d4346868812d3a6e8a1c262
Freeze2222 <opop1094@gmail.com>
2022-04-25 00:16:42 +0300

    Experiment 2

--- app/src/main/java/com/FinalP/finalchat/activities/ChatActivity.java
+++ app/src/main/java/com/FinalP/finalchat/activities/ChatActivity.java
@@ -1,19 +1,27 @@
 package com.FinalP.finalchat.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.FinalP.finalchat.R;
 import com.FinalP.finalchat.adapters.LastMessagesAdapter;
+import com.FinalP.finalchat.fragments.ChatFragment;
+import com.FinalP.finalchat.fragments.ProfileFragment;
+import com.FinalP.finalchat.fragments.UserListFragment;
 import com.FinalP.finalchat.listeners.SimpleListener;
 import com.FinalP.finalchat.models.application.User;
 import com.FinalP.finalchat.services.DatabaseService;
 import com.FinalP.finalchat.services.WrapContentLinearLayoutManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
@@ -27,6 +35,14 @@ public class ChatActivity extends AppCompatActivity {
 
     LastMessagesAdapter adapter;
 
+    BottomNavigationView bottomNavigationView;
+
+    Fragment chatFragment = new ChatFragment();
+    Fragment profileFragment=new ProfileFragment();
+    Fragment addFragment = new UserListFragment();
+
+    Fragment current = chatFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +51,35 @@ public class ChatActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_chat);
 
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.frame_content, chatFragment, "chats")
+                .add(R.id.frame_content, profileFragment, "profile")
+                .add(R.id.frame_content, addFragment,"add")
+                .hide(profileFragment)
+                .hide(addFragment)
+                .commit();
+
         initViews();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        changeFragment(chatFragment);
+                        return true;
+                    case R.id.navigation_profile:
+                        changeFragment(profileFragment);
+                        return true;
+                    case R.id.navigation_addUser:
+                        changeFragment(addFragment);
+                }
+                return false;
+            }
+        });
+
+
         String id = getIntent().getStringExtra("id");
         DatabaseService.getUser(id, new SimpleListener<User>() {
             @Override
@@ -77,6 +121,8 @@ public class ChatActivity extends AppCompatActivity {
         userRecyclerView = findViewById(R.id.userRecyclerView);
 
         userRecyclerView.setLayoutManager(new WrapContentLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
     }
 
     @Override
@@ -91,5 +137,13 @@ public class ChatActivity extends AppCompatActivity {
     protected void onStop() {
         adapter.stopListening();
         super.onStop();
+    }
+    private void changeFragment(Fragment newFragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .hide(current)
+                .show(newFragment)
+                .commit();
+        current = newFragment;
     }
 }
